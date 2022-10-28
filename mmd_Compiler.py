c034848e3d6e8f568f9ad4852d4f2a8158e06e95
Mohammad Naseri <50078819+mamathew98@users.noreply.github.com>
2022-10-28 14:38:00 +0330

    Add files via upload

---
+++ mmd_Compiler.py
@@ -0,0 +1,518 @@
+
+import sys
+import re
+
+def lex(characters, token_exprs):
+    pos = 0
+    tokens = []
+    while pos < len(characters):
+        match = None
+        for token_expr in token_exprs:
+            pattern, tag = token_expr
+            regex = re.compile(pattern)
+            match = regex.match(characters, pos)
+            if match:
+                text = match.group(0)
+                if tag:
+                    token = (text, tag)
+                    tokens.append(token)
+                break
+        if not match:
+            sys.stderr.write('Illegal character: %s\n' % characters[pos])
+            sys.exit(1)
+        else:
+            pos = match.end(0)
+    return tokens
+#============================================
+
+RESERVED = 'RESERVED'
+INT      = 'INT'
+ID       = 'ID'
+
+token_exprs = [
+    (r'[ \n\t]+',              None),
+    (r'#[^\n]*',               None),
+    (r'\:=',                   RESERVED),
+    (r'\(',                    RESERVED),
+    (r'\)',                    RESERVED),
+    (r';',                     RESERVED),
+    (r'\+',                    RESERVED),
+    (r'-',                     RESERVED),
+    (r'\*',                    RESERVED),
+    (r'/',                     RESERVED),
+    (r'<=',                    RESERVED),
+    (r'<',                     RESERVED),
+    (r'>=',                    RESERVED),
+    (r'>',                     RESERVED),
+    (r'!=',                    RESERVED),
+    (r'=',                     RESERVED),
+    (r'if',                    RESERVED),
+    (r'then',                  RESERVED),
+    (r'else',                  RESERVED),
+    (r'while',                 RESERVED),
+    (r'do',                    RESERVED),
+    (r'end',                   RESERVED),
+    (r'[0-9]+',                INT),
+    (r'[A-Za-z][A-Za-z0-9_]*', ID),
+]
+
+def mmd_lex(characters):
+    return lex(characters, token_exprs)
+
+
+#=======================================================
+
+class Equality:
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and \
+               self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+#====================================================
+
+
+class Statement(Equality):
+    pass
+
+class Aexp(Equality):
+    pass
+
+class Bexp(Equality):
+    pass
+
+class AssignStatement(Statement):
+    def __init__(self, name, aexp):
+        self.name = name
+        self.aexp = aexp
+
+    def __repr__(self):
+        return 'AssignStatement(%s, %s)' % (self.name, self.aexp)
+
+    def eval(self, env):
+        value = self.aexp.eval(env)
+        env[self.name] = value
+
+class CompoundStatement(Statement):
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
+    def __repr__(self):
+        return 'CompoundStatement(%s, %s)' % (self.first, self.second)
+
+    def eval(self, env):
+        self.first.eval(env)
+        self.second.eval(env)
+
+class IfStatement(Statement):
+    def __init__(self, condition, true_stmt, false_stmt):
+        self.condition = condition
+        self.true_stmt = true_stmt
+        self.false_stmt = false_stmt
+
+    def __repr__(self):
+        return 'IfStatement(%s, %s, %s)' % (self.condition, self.true_stmt, self.false_stmt)
+
+    def eval(self, env):
+        condition_value = self.condition.eval(env)
+        if condition_value:
+            self.true_stmt.eval(env)
+        else:
+            if self.false_stmt:
+                self.false_stmt.eval(env)
+
+class WhileStatement(Statement):
+    def __init__(self, condition, body):
+        self.condition = condition
+        self.body = body
+
+    def __repr__(self):
+        return 'WhileStatement(%s, %s)' % (self.condition, self.body)
+
+    def eval(self, env):
+        condition_value = self.condition.eval(env)
+        while condition_value:
+            self.body.eval(env)
+            condition_value = self.condition.eval(env)
+
+class IntAexp(Aexp):
+    def __init__(self, i):
+        self.i = i
+
+    def __repr__(self):
+        return 'IntAexp(%d)' % self.i
+
+    def eval(self, env):
+        return self.i
+
+class VarAexp(Aexp):
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return 'VarAexp(%s)' % self.name
+
+    def eval(self, env):
+        if self.name in env:
+            return env[self.name]
+        else:
+            return 0
+
+class BinopAexp(Aexp):
+    def __init__(self, op, left, right):
+        self.op = op
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return 'BinopAexp(%s, %s, %s)' % (self.op, self.left, self.right)
+
+    def eval(self, env):
+        left_value = self.left.eval(env)
+        right_value = self.right.eval(env)
+        if self.op == '+':
+            value = left_value + right_value
+        elif self.op == '-':
+            value = left_value - right_value
+        elif self.op == '*':
+            value = left_value * right_value
+        elif self.op == '/':
+            value = left_value / right_value
+        else:
+            raise RuntimeError('unknown operator: ' + self.op)
+        return value
+
+class RelopBexp(Bexp):
+    def __init__(self, op, left, right):
+        self.op = op
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return 'RelopBexp(%s, %s, %s)' % (self.op, self.left, self.right)
+
+    def eval(self, env):
+        left_value = self.left.eval(env)
+        right_value = self.right.eval(env)
+        if self.op == '<':
+            value = left_value < right_value
+        elif self.op == '<=':
+            value = left_value <= right_value
+        elif self.op == '>':
+            value = left_value > right_value
+        elif self.op == '>=':
+            value = left_value >= right_value
+        elif self.op == '=':
+            value = left_value == right_value
+        elif self.op == '!=':
+            value = left_value != right_value
+        else:
+            raise RuntimeError('unknown operator: ' + self.op)
+        return value
+
+#======================================================
+#======================================================
+class Result:
+    def __init__(self, value, pos):
+        self.value = value
+        self.pos = pos
+
+    def __repr__(self):
+        return 'Result(%s, %d)' % (self.value, self.pos)
+
+class Parser:
+    def __add__(self, other):
+        return Concat(self, other)
+
+    def __mul__(self, other):
+        return Exp(self, other)
+
+    def __or__(self, other):
+        return Alternate(self, other)
+
+    def __xor__(self, function):
+        return Process(self, function)
+
+class Tag(Parser):
+    def __init__(self, tag):
+        self.tag = tag
+
+    def __call__(self, tokens, pos):
+        if pos < len(tokens) and tokens[pos][1] is self.tag:
+            return Result(tokens[pos][0], pos + 1)
+        else:
+            return None
+
+class Reserved(Parser):
+    def __init__(self, value, tag):
+        self.value = value
+        self.tag = tag
+
+    def __call__(self, tokens, pos):
+        if pos < len(tokens) and \
+           tokens[pos][0] == self.value and \
+           tokens[pos][1] is self.tag:
+            return Result(tokens[pos][0], pos + 1)
+        else:
+            return None
+
+class Concat(Parser):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def __call__(self, tokens, pos):
+        left_result = self.left(tokens, pos)
+        if left_result:
+            right_result = self.right(tokens, left_result.pos)
+            if right_result:
+                combined_value = (left_result.value, right_result.value)
+                return Result(combined_value, right_result.pos)
+        return None
+
+class Exp(Parser):
+    def __init__(self, parser, separator):
+        self.parser = parser
+        self.separator = separator
+
+    def __call__(self, tokens, pos):
+        result = self.parser(tokens, pos)
+
+        def process_next(parsed):
+            (sepfunc, right) = parsed
+            return sepfunc(result.value, right)
+        next_parser = self.separator + self.parser ^ process_next
+
+        next_result = result
+        while next_result:
+            next_result = next_parser(tokens, result.pos)
+            if next_result:
+                result = next_result
+        return result            
+
+class Alternate(Parser):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def __call__(self, tokens, pos):
+        left_result = self.left(tokens, pos)
+        if left_result:
+            return left_result
+        else:
+            right_result = self.right(tokens, pos)
+            return right_result
+
+class Opt(Parser):
+    def __init__(self, parser):
+        self.parser = parser
+
+    def __call__(self, tokens, pos):
+        result = self.parser(tokens, pos)
+        if result:
+            return result
+        else:
+            return Result(None, pos)
+
+class Rep(Parser):
+    def __init__(self, parser):
+        self.parser = parser
+
+    def __call__(self, tokens, pos):
+        results = []
+        result = self.parser(tokens, pos)
+        while result:
+            results.append(result.value)
+            pos = result.pos
+            result = self.parser(tokens, pos)
+        return Result(results, pos)
+
+class Process(Parser):
+    def __init__(self, parser, function):
+        self.parser = parser
+        self.function = function
+
+    def __call__(self, tokens, pos):
+        result = self.parser(tokens, pos)
+        if result:
+            result.value = self.function(result.value)
+            return result
+
+class Lazy(Parser):
+    def __init__(self, parser_func):
+        self.parser = None
+        self.parser_func = parser_func
+
+    def __call__(self, tokens, pos):
+        if not self.parser:
+            self.parser = self.parser_func()
+        return self.parser(tokens, pos)
+
+class Phrase(Parser):
+    def __init__(self, parser):
+        self.parser = parser
+
+    def __call__(self, tokens, pos):
+        result = self.parser(tokens, pos)
+        if result and result.pos == len(tokens):
+            return result
+        else:
+            return None
+# ===============================================
+
+# Basic parsers
+def keyword(kw):
+    return Reserved(kw, RESERVED)
+
+num = Tag(INT) ^ (lambda i: int(i))
+id = Tag(ID)
+
+# Top level parser
+def mmd_parse(tokens):
+    ast = parser()(tokens, 0)
+    return ast
+
+def parser():
+    return Phrase(stmt_list())    
+
+# Statements
+def stmt_list():
+    separator = keyword(';') ^ (lambda x: lambda l, r: CompoundStatement(l, r))
+    return Exp(stmt(), separator)
+
+def stmt():
+    return assign_stmt() | \
+           if_stmt()     | \
+           while_stmt()
+
+def assign_stmt():
+    def process(parsed):
+        ((name, _), exp) = parsed
+        return AssignStatement(name, exp)
+    return id + keyword(':=') + aexp() ^ process
+
+def if_stmt():
+    def process(parsed):
+        (((((_, condition), _), true_stmt), false_parsed), _) = parsed
+        if false_parsed:
+            (_, false_stmt) = false_parsed
+        else:
+            false_stmt = None
+        return IfStatement(condition, true_stmt, false_stmt)
+    return keyword('if') + bexp() + \
+           keyword('then') + Lazy(stmt_list) + \
+           Opt(keyword('else') + Lazy(stmt_list)) + \
+           keyword('end') ^ process
+
+def while_stmt():
+    def process(parsed):
+        ((((_, condition), _), body), _) = parsed
+        return WhileStatement(condition, body)
+    return keyword('while') + bexp() + \
+           keyword('do') + Lazy(stmt_list) + \
+           keyword('end') ^ process
+
+# Boolean expressions
+def bexp():
+    return precedence(bexp_term(),
+                      bexp_precedence_levels,
+                      process_logic)
+
+def bexp_term():
+    return bexp_not()   | \
+           bexp_relop() | \
+           bexp_group()
+
+def bexp_not():
+    return keyword('not') + Lazy(bexp_term) ^ (lambda parsed: NotBexp(parsed[1]))
+
+def bexp_relop():
+    relops = ['<', '<=', '>', '>=', '=', '!=']
+    return aexp() + any_operator_in_list(relops) + aexp() ^ process_relop
+
+def bexp_group():
+    return keyword('(') + Lazy(bexp) + keyword(')') ^ process_group
+
+# Arithmetic expressions
+def aexp():
+    return precedence(aexp_term(),
+                      aexp_precedence_levels,
+                      process_binop)
+
+def aexp_term():
+    return aexp_value() | aexp_group()
+
+def aexp_group():
+    return keyword('(') + Lazy(aexp) + keyword(')') ^ process_group
+           
+def aexp_value():
+    return (num ^ (lambda i: IntAexp(i))) | \
+           (id  ^ (lambda v: VarAexp(v)))
+
+#  binary operator expressions (aexp and bexp)
+def precedence(value_parser, precedence_levels, combine):
+    def op_parser(precedence_level):
+        return any_operator_in_list(precedence_level) ^ combine
+    parser = value_parser * op_parser(precedence_levels[0])
+    for precedence_level in precedence_levels[1:]:
+        parser = parser * op_parser(precedence_level)
+    return parser
+
+# binary and relational operators
+def process_binop(op):
+    return lambda l, r: BinopAexp(op, l, r)
+
+def process_relop(parsed):
+    ((left, op), right) = parsed
+    return RelopBexp(op, left, right)
+
+def process_logic(op):
+    if op == 'and':
+        return lambda l, r: AndBexp(l, r)
+    elif op == 'or':
+        return lambda l, r: OrBexp(l, r)
+    else:
+        raise RuntimeError('unknown logic operator: ' + op)
+
+def process_group(parsed):
+    ((_, p), _) = parsed
+    return p
+
+def any_operator_in_list(ops):
+    op_parsers = [keyword(op) for op in ops]
+    parser = reduce(lambda l, r: l | r, op_parsers)
+    return parser
+
+# Operator keywords and precedence levels
+aexp_precedence_levels = [
+    ['*', '/'],
+    ['+', '-'],
+]
+
+bexp_precedence_levels = [
+    ['and'],
+    ['or'],
+]
+
+#=======================================================
+#=======================================================
+
+
+if __name__ == '__main__':
+   
+    filename = 'hello.mmd'
+    text = open(filename).read()
+    tokens = mmd_lex(text)
+    parse_result = mmd_parse(tokens)
+    if not parse_result:
+        sys.stderr.write('Parse error!\n')
+        sys.exit(1)
+    ast = parse_result.value
+    env = {}
+    ast.eval(env)
+
+    sys.stdout.write('Final variable values:\n')
+    for name in env:
+        sys.stdout.write('%s: %s\n' % (name, env[name]))
+
